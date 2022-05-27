aa622487dd3f69629a62729c151a9ddac4f4f60a
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-05-27 12:46:05 -0700

    reactJSX

--- React-Fundamentals/my-react-app/src/App.js
+++ React-Fundamentals/my-react-app/src/App.js
@@ -1,30 +1,61 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+import React from "react";
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi mom + dad</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {welcomeList.map((w) => `${w.greeting} ${w.title} `)}
+        {welcome.greeting} {welcome.title}
+      </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
 
-export class Spotify extends Component {
-  render() {
-    return <div> Test </div>;
-  }
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hi mom + dad</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
