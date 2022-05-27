6fd2f15671cd095f6d2b14572dbbcb2dfb016b8d
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-05-27 13:36:19 -0700

    React Component Instatiation

--- React-Fundamentals/my-react-app/src/App.js
+++ React-Fundamentals/my-react-app/src/App.js
@@ -21,24 +21,41 @@ const list = [
   },
 ];
 
-const welcome = {
-  greeting: "Hey",
-  title: "React",
-};
-
+const list2 = [
+  {
+    title: "Poop de scoop",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+];
 function App() {
   return (
     <div>
-      <h1>
-        {welcomeList.map((w) => `${w.greeting} ${w.title} `)}
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      <hr />
+      <List list={list} />
+      <List list={list2} />
     </div>
   );
 }
 
+function List(props) {
+  return props.list.map((item) => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title} </a>
+      </span>
+      <span>{item.author} </span>
+      <span>{item.num_comments} </span>
+      <span>{item.points} </span>
+    </div>
+  ));
+}
 // function App() {
 //   return (
 //     <div className="App">
